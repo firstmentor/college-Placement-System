827368fc4f94e0acff987b38eca2d732bc7645db
firstmentor <jainvikas887@gmail.com>
2025-06-16 02:33:18 +0530

    fgghffghhgf

--- models/HodAttendance.js
+++ models/HodAttendance.js
@@ -3,17 +3,19 @@ const mongoose = require("mongoose");
 const hodAttendanceSchema = new mongoose.Schema({
   hodId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "hod",
+    ref: "Hod",
     required: true,
   },
   date: {
-    type: String, // Store as 'YYYY-MM-DD'
+    type: String, // Format: YYYY-MM-DD
     required: true,
   },
   time: {
-    type: String, // Store time of marking
+    type: String, // Format: HH:mm:ss
     required: true,
-  },
+  }
+}, {
+  timestamps: true
 });
 
 hodAttendanceSchema.index({ hodId: 1, date: 1 }, { unique: true });
